e3c76e874a37b20306c0e8209934e47667ca1832
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-09 17:03:08 +0530

    list and update api

--- server.js
+++ server.js
@@ -1,46 +1,73 @@
-import express from "express"
-import pgClient from "pg"
-import dotev from "dotenv"
-import cors from "cors"
+import express from "express";
+import pgClient from "pg";
+import dotev from "dotenv";
+import cors from "cors";
 
-const {Client}  = pgClient
+const { Client } = pgClient;
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-dotev.config()
-app.use(cors({origin: "*"}))
-app.use(express.json())
-
+dotev.config();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 //psql client setup
 const client = new Client({
-    user: process.env.USER,
-    host: "localhost",
-    database: process.env.DB,
-    password: process.env.PASSWORD,
-    port: 5432
-})
+  user: process.env.USER,
+  host: "localhost",
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  port: 5432,
+});
 
-client.connect()
+client
+  .connect()
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.error("Error connecting to the database", err.stack));
 
 app.post("/", async (req, res) => {
-  const { title, description, best_selling } = req.body
-  
+  const { title, description, best_selling } = req.body;
+
   try {
     const result = await client.query(
-      'INSERT INTO books (title, description , best_selling) VALUES ($1, $2, $3) RETURNING *', [title, description, best_selling]
-    )
+      "INSERT INTO books (title, description , best_selling) VALUES ($1, $2, $3) RETURNING *",
+      [title, description, best_selling]
+    );
 
-    res.status(201).json({message: "Book Created", book: result.rows?.[0]})
-    
+    res.status(201).json({ message: "Book Created", book: result.rows?.[0] });
   } catch (error) {
-      res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
   }
-  })
+});
 
-app.listen(port,() => {
-    console.log(`Server is running on port ${port}`)
+app.put("/:id", async (req, res) => {
+  
+  const { id } = req.params
+    const { title, description, best_selling } = req.body;
+  try {
+    const result = await client.query("UPDATE books SET title = $1, description =$2, best_selling=$3 WHERE id = $4 RETURNING *", [title, description, best_selling, id])
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({message: `Book not found with id: ${id}`})
+    }
+ 
+    res.status(200).json({ message: "book updated", books: result.rows?.[0] });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await client.query("SELECT * FROM books")
+
+    res.status(200).json({ message: "success", books: result.rows });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
