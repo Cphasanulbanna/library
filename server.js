cc01c9f878b18ad4ae1bcd6de3be82ec05da6793
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-09 16:50:24 +0530

    created add new book api

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express"
 import pgClient from "pg"
+import dotev from "dotenv"
+import cors from "cors"
 
 const {Client}  = pgClient
-import dotev from "dotenv"
 
 const app = express()
-const port = 3000
+const port = 3001
 
 dotev.config()
+app.use(cors({origin: "*"}))
 app.use(express.json())
 
 
@@ -17,13 +19,28 @@ const client = new Client({
     host: "localhost",
     database: process.env.DB,
     password: process.env.PASSWORD,
-    port: port
+    port: 5432
 })
 
 client.connect()
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.error("Error connecting to the database", err.stack));
 
-app.listen(() => {
+app.post("/", async (req, res) => {
+  const { title, description, best_selling } = req.body
+  
+  try {
+    const result = await client.query(
+      'INSERT INTO books (title, description , best_selling) VALUES ($1, $2, $3) RETURNING *', [title, description, best_selling]
+    )
+
+    res.status(201).json({message: "Book Created", book: result.rows?.[0]})
+    
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+  })
+
+app.listen(port,() => {
     console.log(`Server is running on port ${port}`)
 })
