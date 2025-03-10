ff4b5a2335cbac13a176a8bda6904cb90a1451b6
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-10 09:06:25 +0530

    bug fixes

--- middleware.js
+++ middleware.js
@@ -1,18 +1,30 @@
 import jwt from "jsonwebtoken";
-import { client } from "./server";
+import { client } from "./server.js";
 
 export const checkRole = (roleName) => {
   return async (req, res, next) => {
-    const token = req.headers.authorization.split(" ")?.[1];
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(403).send({ message: "Unauthorized, no token provided" });
+        
+    }
+      const token = authHeader.split(" ")?.[1];
+      
 
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const userId = decoded.userId;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        const userId = decoded.userId;
+        
 
       const result = await client.query(
-        "SELECT role_name FROM roles INNER JOIN user_roles ON roles.id = user_roles.role_id WHERE user_roles.id = $1",
+        "SELECT role_name FROM roles INNER JOIN user_roles ON roles.id = user_roles.role_id WHERE user_roles.user_id = $1",
         [userId]
       );
+        
+        if (!result.rows.length) {
+            return res.status(401).json({message: "not authorized"})
+        }
 
       const roles = result.rows?.map((row) => row.role_name);
       if (roles?.includes(roleName)) {
@@ -21,7 +33,7 @@ export const checkRole = (roleName) => {
         return res.status(403).send("Access denied");
       }
     } catch (error) {
-      return res.status(401).send("Invalid token");
+        return res.status(401).send({ error: error.message, message: "error" });
     }
   };
 };
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from "express";
 import pgClient from "pg";
 import dotev from "dotenv";
 import cors from "cors";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { hashPassword } from "./encryption.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import { checkRole } from "./middleware.js";
 
 const { Client } = pgClient;
 
@@ -92,7 +93,7 @@ app.delete("/:id", async (req, res) => {
 });
 
 app.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password , role="User"} = req.body;
   try {
     if (!email || !password)
       return res
@@ -117,7 +118,7 @@ app.post("/signup", async (req, res) => {
     const userId = result.rows?.[0]?.id;
     await client.query(
       "INSERT INTO user_roles(user_id, role_id) VALUES ($1, (SELECT id FROM roles WHERE role_name = $2))",
-      [userId, "User"]
+      [userId, role]
     );
 
     res.status(201).json({ message: "User created" });
@@ -133,30 +134,38 @@ app.post("/login", async (req, res) => {
       return res
         .status(400)
         .json({ message: "Email and Password is required" });
-    
+
     const { rows: userRow } = await client.query(
-      'SELECT * FROM users WHERE email = $1', [email]
-    )
-        const user = userRow?.[0]
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    const user = userRow?.[0];
     if (!user) {
-      return res.status(404).json({message: "User not found"})
+      return res.status(404).json({ message: "User not found" });
     }
 
-
-    const decodedPassword = await bcrypt.compare(password, user.password)
+    const decodedPassword = await bcrypt.compare(password, user.password);
     if (!decodedPassword) {
-       return res.status(401).json({message: 'Invalid password'});
+      return res.status(401).json({ message: "Invalid password" });
     }
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })
-    
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
     await client.query(
-      'INSERT INTO sessions (user_id, token) VALUES ($1, $2)', [user.id, token]
-    )
+      "INSERT INTO sessions (user_id, token) VALUES ($1, $2)",
+      [user.id, token]
+    );
 
-    const {rows: roleObject} = await client.query('SELECT role_name FROM roles INNER JOIN user_roles on roles.id = user_roles.role_id WHERE user_roles.user_id  =$1', [user.id])
+    const { rows: roleObject } = await client.query(
+      "SELECT role_name FROM roles INNER JOIN user_roles on roles.id = user_roles.role_id WHERE user_roles.user_id  =$1",
+      [user.id]
+    );
 
-    res.status(200).json({token, email: user.email, role: roleObject?.[0]?.role_name})
+    res
+      .status(200)
+      .json({ token, email: user.email, role: roleObject?.[0]?.role_name });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -178,6 +187,10 @@ app.post("/role", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+});
+
+app.get("/admin-protected", checkRole("Admin"), async (req, res) => {
+  res.status(200).json({ message: "This is admin route" });
 });
 
 app.listen(port, () => {
